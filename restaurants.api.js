fcda104a73ab3b3557b952857fc2f59cf26016aa
Ajla Velagic <ajvelagic@gmail.com>
2020-09-14 16:36:50 -0400

    added get method for retrieving params

--- restaurants.api.js
+++ restaurants.api.js
@@ -5,11 +5,22 @@ const express = require("express");
 const routes = express.Router();
 const pool = require("./connection");
 
+// routes.get("/restaurants", (req, res) => {
+//   let queryString = `SELECT * FROM restaurants ORDER BY RANDOM() LIMIT 15`;
+//   pool.query(queryString).then((response) => {
+//     res.json(response.rows);
+//   });
+// });
+
 routes.get("/restaurants", (req, res) => {
-  let queryString = `SELECT * FROM restaurants WHERE city = 'gr' ORDER BY RANDOM() LIMIT 15`;
+  let city = req.query.city;
+  console.log(req.query.city);
+  let queryString = `SELECT * FROM restaurants WHERE city='${city}'`;
   pool.query(queryString).then((response) => {
     res.json(response.rows);
   });
+
 });
+
 
 module.exports = routes;
